d43219ff92c2b2164d32debe123a1b10d8728119
Melvin Zehl <mhzehl@gmail.com>
2017-01-13 16:19:28 +0100

    Add new book method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,23 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :image_url)
+  end
 end

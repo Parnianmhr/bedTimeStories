95c3221751eb8396d2fd9687059ad45354ba5cbe
parnianmhr <parnian@gmail.com>
2017-01-13 13:05:19 +0100

    Create books and page views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 Page.delete_all
 Book.delete_all
 
-page1 = Page.create( {image_url: "photo1", text: "text1 test"})
-page2 = Page.create( {image_url: "photo2", text: "text2 test"})
-page3 = Page.create( {image_url: "photo3", text: "text3 test"})
+page1 = Page.create( {image_url: "http://bit.ly/2j7yiR6", text: "text1 test"})
+page2 = Page.create( {image_url: "http://bit.ly/2j7yiR6", text: "text2 test"})
+page3 = Page.create( {image_url: "http://bit.ly/2j7yiR6", text: "text3 test"})
 
-book1 = Book.create( {image_url: "phototitle1", title: "title1 test"})
+book1 = Book.create( {image_url: "http://bit.ly/2j7yiR6", title: "title1 test"})
 # book2 = Book.create( {image_url: "phototitle2", title: "title2 test"})
 
 book1.pages << [page1]
